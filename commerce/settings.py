b600f71506d05e19b0c668ee2666489393a765a2
Matheus2608 <matheuslacerdadasilveira@gmail.com>
2022-05-14 23:30:58 -0300

    Update ALLOWED_HOSTS with site and development server URL

--- commerce/settings.py
+++ commerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6ps8j!crjgrxt34cqbqn7x&b3y%(fn
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mafiawe.herokuapp.com','127.0.0.1']
 
 
 # Application definition
